9502a56820d9ab1111113db9da381bfd832ec4c3
Eddie Garmon <eddie.garmon@volvo.com>
2024-04-13 19:42:17 -0400

    add file system locators

---
+++ Source/SmartPaths/Storage/FileSystem.cs
@@ -0,0 +1,16 @@
+﻿using SmartPaths.Storage.Disk;
+using SmartPaths.Storage.Ram;
+
+namespace SmartPaths.Storage;
+
+public static class FileSystem
+{
+
+    private static DiskFileSystem? _disk;
+    private static RamFileSystem? _ram;
+
+    public static DiskFileSystem Disk => _disk ??= new DiskFileSystem();
+
+    public static RamFileSystem Ram => _ram ??= new RamFileSystem();
+
+}
