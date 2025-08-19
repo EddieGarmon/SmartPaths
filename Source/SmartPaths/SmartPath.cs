4cecd16d168c148b4d90eb5a5fb78b6db37bb88e
Eddie Garmon <eddie_garmon@hotmail.com>
2025-08-19 14:20:06 -0400

    update parsing methods

--- Source/SmartPaths/SmartPath.cs
+++ Source/SmartPaths/SmartPath.cs
@@ -14,39 +14,41 @@ public static class SmartPath
 
     public static AbsolutePath ParseAbsolute(string path) {
         (PathType pathType, Match _) = PathPatterns.DeterminePathType(path);
-        if (pathType == PathType.Relative) {
-            throw new Exception($"Path [{path}] is relative.");
-        }
-        return path.LastIndexOfAny(['\\', '/']) == path.Length - 1 ? new AbsoluteFolderPath(path) : new AbsoluteFilePath(path);
+        return pathType switch {
+            PathType.Absolute => path.LastIndexOfAny(['\\', '/']) == path.Length - 1 ? new AbsoluteFolderPath(path) : new AbsoluteFilePath(path),
+            _ => throw new Exception($"Path [{path}] is {pathType} not Absolute.")
+        };
     }
 
     public static IFilePath ParseFile(string path) {
         (PathType pathType, Match _) = PathPatterns.DeterminePathType(path);
-        if (pathType == PathType.Relative) {
-            return new RelativeFilePath(path);
-        }
-        return new AbsoluteFilePath(path);
+        return pathType switch {
+            PathType.Relative => new RelativeFilePath(path),
+            PathType.Absolute => new AbsoluteFilePath(path),
+            _ => throw new Exception($"Invalid path for file: {path}")
+        };
     }
 
     public static IFolderPath ParseFolder(string path) {
         (PathType pathType, Match _) = PathPatterns.DeterminePathType(path);
-        if (pathType == PathType.Relative) {
-            return new RelativeFolderPath(path);
-        }
-        return new AbsoluteFolderPath(path);
+        return pathType switch {
+            PathType.Relative => new RelativeFolderPath(path),
+            PathType.Absolute => new AbsoluteFolderPath(path),
+            _ => throw new Exception($"Invalid path for folder: {path}")
+        };
     }
 
     public static RelativePath ParseRelative(string path) {
         (PathType pathType, Match _) = PathPatterns.DeterminePathType(path);
-        if (pathType != PathType.Relative) {
-            throw new Exception($"Path [{path}] is absolute.");
-        }
-        return path.LastIndexOfAny(['\\', '/']) == path.Length - 1 ? new RelativeFolderPath(path) : new RelativeFilePath(path);
+        return pathType switch {
+            PathType.Relative => path.LastIndexOfAny(['\\', '/']) == path.Length - 1 ? new RelativeFolderPath(path) : new RelativeFilePath(path),
+            _ => throw new Exception($"Path [{path}] is {pathType} not Relative.")
+        };
     }
 
-    public static bool TryParse(string? path, [NotNullWhen(true)] out IPath? smartPath) {
-        if (path is null) {
-            smartPath = null;
+    public static bool TryParse(string path, [NotNullWhen(true)] out IPath? smartPath) {
+        smartPath = null;
+        if (string.IsNullOrWhiteSpace(path)) {
             return false;
         }
         try {
@@ -55,6 +57,42 @@ public static class SmartPath
         } catch (Exception) {
             smartPath = null;
             return false;
+        }
+    }
+
+    public static bool TryParseFile(string path, [NotNullWhen(true)] out IPath? filePath) {
+        filePath = null;
+        if (string.IsNullOrWhiteSpace(path)) {
+            return false;
+        }
+        (PathType pathType, Match _) = PathPatterns.DeterminePathType(path);
+        switch (pathType) {
+            case PathType.Relative:
+                filePath = new RelativeFilePath(path);
+                return true;
+            case PathType.Absolute:
+                filePath = new AbsoluteFilePath(path);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public static bool TryParseFolder(string path, [NotNullWhen(true)] out IPath? folderPath) {
+        folderPath = null;
+        if (string.IsNullOrWhiteSpace(path)) {
+            return false;
+        }
+        (PathType pathType, Match _) = PathPatterns.DeterminePathType(path);
+        switch (pathType) {
+            case PathType.Relative:
+                folderPath = new RelativeFolderPath(path);
+                return true;
+            case PathType.Absolute:
+                folderPath = new AbsoluteFolderPath(path);
+                return true;
+            default:
+                return false;
         }
     }
 
