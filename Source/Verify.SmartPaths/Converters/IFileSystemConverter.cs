99cd7af2b45b909547ce339f476177d5ce89caf6
Eddie Garmon <eddie_garmon@hotmail.com>
2025-08-29 11:50:48 -0400

    Renamed project for Verify ecosystem. Fixed support for automatic plugin detection

---
+++ Source/Verify.SmartPaths/Converters/IFileSystemConverter.cs
@@ -0,0 +1,13 @@
+﻿using SmartPaths.Storage;
+
+namespace VerifyTests.Converters;
+
+public class IFileSystemConverter : WriteOnlyJsonConverter<IFileSystem>
+{
+
+    public override void Write(VerifyJsonWriter writer, IFileSystem value) {
+        //write out a list of all files?
+        throw new NotImplementedException();
+    }
+
+}
--- Tests/Verify.SmartPaths.Tests/FileFormatSupportTests.cs
+++ Tests/Verify.SmartPaths.Tests/FileFormatSupportTests.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using SmartPaths.Storage;
 
-namespace SmartPaths;
+namespace VerifySmartPaths;
 
 public class FileFormatSupportTests
 {
@@ -12,7 +12,7 @@ public class FileFormatSupportTests
         Ledger ledger = await fileSystem.StartLedger();
 
         //manipulate file system
-        await using Stream? resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SmartPaths.Files.icon.png");
+        await using Stream? resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("VerifySmartPaths.Files.icon.png");
         if (resourceStream is null) {
             Assert.Fail("Cant find test resource: icon.png");
         }
@@ -22,6 +22,7 @@ public class FileFormatSupportTests
         }
 
         //verify ledger
+        //NB: we are using Verify.ImageSharp - Should see picture delta
         await Verify(ledger).UseDirectory(@"Snapshots");
     }
 
@@ -31,7 +32,7 @@ public class FileFormatSupportTests
         Ledger ledger = await fileSystem.StartLedger();
 
         //manipulate file system
-        await using Stream? resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SmartPaths.Files.example.pdf");
+        await using Stream? resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("VerifySmartPaths.Files.example.pdf");
         if (resourceStream is null) {
             Assert.Fail("Cant find test resource: example.pdf");
         }
